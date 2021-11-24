467d3e8c22586a30b40fd61fa1ba499f36be62a3
justin639 <justin639@gmail.com>
2021-11-25 01:03:10 +0900

    first commit

---
+++ project_2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// demand paging system
+// page replacement
+// MIN : 
+// FIFO : first in first out
+// LRU : 
+// LFU : 
+// WS Memory Management : 
+// track changes in memory residence set, page fault 
+
+// inputs
+// Page frame, window size
+// init page frames are empty
+// N(# page per process : max 100) | M(# page frame : max 20; not used in WS) 
+// W(window size : max 100) | K(page reference string length : max 1000)
+// N M W K
+// page reference string : page num start from 0
+
+// output
+// MIN
+// 메모리 상태 변화 과정 (page fault 발생 위치 표시)
+// 총 page fault 횟수
+// FIFO
+// ...
+
+// ---------- Outline --------------
+//
+// file read & set initial queue
+//
+// show result
+// ---------------------------------
+
+#define MAX 200
+#define QUENUM 4
+#define FALSE 0
+#define TRUE 1
+
+int main(){
+
+}
