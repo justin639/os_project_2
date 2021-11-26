978a1f97b23ada4aa1fd7740f17ed1537fe8522f
justin639 <justin639@gmail.com>
2021-11-26 22:38:48 +0900

    Add replacemet method comments v.0.0.3

--- project_2.c
+++ project_2.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// demand paging system
+// demand paging system : partition the program into same size blocks(pages)
+// PMT
+// residence bit ... page frame number
 // page replacement
-// MIN : 
-// FIFO : first in first out
-// LRU : 
-// LFU : 
-// WS Memory Management : 
+// MIN : Minimize page fault frequency *
+// FIFO : first in first out - queue
+// LRU : Least Recently Used - stack
+// LFU : Least Frequently Used - stack
+// process -> pages
+// pages -> Memory(page frames)
+// page fault -> no more page frame left -> replace (for fixed)
+// 
+// WS Memory Management : working set *
+// after window size(WS) page gets out - not page fault
+// page fault -> page new input(for time interval WS)
 // track changes in memory residence set, page fault 
+// tie-break rules
 
 // inputs
 // Page frame, window size
@@ -27,16 +36,20 @@
 
 // ---------- Outline --------------
 //
-// file read & set initial queue
-//
+// file read & set initial queue and stacks
+// 
 // show result
 // ---------------------------------
 
 #define MAX 200
-#define QUENUM 4
 #define FALSE 0
 #define TRUE 1
 
-int main(){
+int main(int argc, char **argv){
 
+    // Local Variables
+    int page_num, page_frame_num, window_size, reference_string_len;
+    int time_count = 0;
+
+    return 0;
 }
